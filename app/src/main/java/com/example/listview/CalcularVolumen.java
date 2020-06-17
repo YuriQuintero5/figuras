989fce7833bf24358d322450e876e72f17d5eaf7
Yuri Quintero <ingenieraquintero@gmail.com>
2020-06-17 06:13:00 -0500

    Vista calcular volumen (Esfera)

---
+++ app/src/main/java/com/example/listview/CalcularVolumen.java
@@ -0,0 +1,66 @@
+package com.example.listview;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Pattern;
+
+public class CalcularVolumen extends AppCompatActivity {
+    private TextView titulo;
+    private EditText txtRadio;
+    private Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calcular_volumen);
+        titulo = findViewById(R.id.lblTitulo);
+        txtRadio = findViewById(R.id.txtRadio);
+        titulo.setText("ESFERA");
+
+        // getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+    }
+    public void calcular(View v){
+        String sRadio = txtRadio.getText().toString();
+        double radio = 0f;
+        if(isNumeric(sRadio)) {
+            radio = Float.parseFloat(sRadio);
+            Double volumen = (4/3) * radio * radio * Math.PI;
+            openDialog("ESFERA", "Volumen: "+volumen.toString());
+        }
+    }
+    public void openDialog(String title, String texto) {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage(texto);
+        builder1.setCancelable(false);
+
+        builder1.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+    public void limpiar(View v){
+        txtRadio.setText("");
+    }
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
+    }
+}
--- app/src/main/java/com/example/listview/OpcionVolumen.java
+++ app/src/main/java/com/example/listview/OpcionVolumen.java
@@ -1,16 +1,53 @@
 package com.example.listview;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OpcionVolumen extends AppCompatActivity {
+    private ListView Lista_Opciones;
+    private String [] opciones;
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.operacion_volumen);
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+
+        Lista_Opciones = findViewById(R.id.listOpciones);
+        opciones = getResources().getStringArray(R.array.opcionesVolumenes);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, opciones);
+        Lista_Opciones.setAdapter(adapter);
+        Lista_Opciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        intent = new Intent(OpcionVolumen.this, CalcularVolumen.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(OpcionVolumen.this, CalcularVolumen.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent = new Intent(OpcionVolumen.this, CalcularVolumen.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        intent = new Intent(OpcionVolumen.this, CalcularVolumen.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
     // And override this method
     @Override
